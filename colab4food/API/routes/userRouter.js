506ba1b19e6db6f1e0f4a14ad1f0e25c3347d24c
Gabriel <guigas78@gmail.com>
2022-05-25 00:09:29 +0100

    Routing and controllers
    Added the link between them.
    Added indexRoute.js to load all routes.
    Added getUserById querie to test using queries with parameters.

--- colab4food/API/routes/userRouter.js
+++ colab4food/API/routes/userRouter.js
@@ -2,5 +2,6 @@ const userController = require('../controllers/userController');
 const router = require('express').Router();
 
 router.get('/getAllUsers', userController.getAllUsers);
+router.get('/getUserById:id_user', userController.getUserById);
 
 module.exports = router;
