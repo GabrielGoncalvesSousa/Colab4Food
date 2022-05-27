8dc5210bad8d42c6ba61c5f611e217025306c34d
Gabriel <guigas78@gmail.com>
2022-05-27 11:49:48 +0100

    Tweaking (db & seeds) & diagrams added

--- colab4food/API/models/tipo_servico.js
+++ colab4food/API/models/tipo_servico.js
@@ -1,29 +1,33 @@
 const Sequelize = require('sequelize');
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('tipo_servico', {
-    id_tipoServico: {
-      autoIncrement: true,
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true
-    },
-    nomeTipoServico: {
-      type: DataTypes.STRING(45),
-      allowNull: false
-    }
-  }, {
-    sequelize,
-    tableName: 'tipo_servico',
-    timestamps: false,
-    indexes: [
-      {
-        name: "PRIMARY",
-        unique: true,
-        using: "BTREE",
-        fields: [
-          { name: "id_tipoServico" },
-        ]
+  return sequelize.define(
+    'tipo_servico',
+    {
+      id_tipoServico: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
       },
-    ]
-  });
+      nomeTipoServico: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'tipo_servico',
+      timestamps: false,
+      indexes: [
+        {
+          name: 'PRIMARY',
+          unique: true,
+          using: 'BTREE',
+          fields: [
+            { name: 'id_tipoServico' },
+          ],
+        },
+      ],
+    }
+  );
 };
